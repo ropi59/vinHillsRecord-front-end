f291ad8628f78c22d0f4344477a9dacf612274fe
ropi59 <mrseed@hotmail.com>
2022-03-22 15:07:25 +0100

    [about page] about page done

---
+++ src/components/Members.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import yellow_vinyl from "./../assets/yellow_vinyl_icon.png"
+
+const Members = ({members}) => {
+    
+    return (
+        <div className='members'>
+            <div className='left-part'>
+                <div className='picture'>
+                    <img src={members.portraitURL} alt={members.name} className='portraitPic'></img>
+                </div>
+                <div className='memberDescription'>
+                    <h3>{members.name}: {members.role}</h3>
+                    <p>{members.description}</p>
+                </div>
+            </div>
+            <div className='right-part'>
+                <div className='buttonDiv'>
+                    <button className='playlist'>{members.selectionButtonContent}</button>
+                </div>
+                <div className="iconDiv">
+                    <img src={yellow_vinyl} className="iconContainer"></img>
+                </div>
+            </div>
+            
+        </div>
+    );
+};
+
+export default Members;
--- src/pages/About.js
+++ src/pages/About.js
@@ -1,16 +1,64 @@
+import axios from "axios";
+import { useEffect, useState } from "react";
+import Members from "../components/Members";
+import ourTeamPic from "./../assets/pictures/our-team.jpg"
+import handiPic from "./../assets/pictures/handy-friendly.jpg"
+import selectionPic from "./../assets/pictures/selection.jpg"
+import expertPic from "./../assets/pictures/expert.jpg"
+
 const About = () => {
+  const [data, setData] = useState([]);
+    const [playOnce, setPlayOnce] = useState(true);
+
+    useEffect(() => {
+        if(playOnce){
+            axios.get(
+                "http://localhost:8080/crew"
+            )
+            .then((res) => {
+              setData(res.data);
+              setPlayOnce(false);
+            });
+        }
+    })
   return (
-    <div>  
-      <h3>À propos</h3>
-      <p>
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
-        tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
-        veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
-        commodo consequat. Duis aute irure dolor in reprehenderit in voluptate
-        velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint
-        occaecat cupidatat non proident, sunt in culpa qui officia deserunt
-        mollit anim id est laborum.
-      </p>
+    <div className="about"> 
+      <div className="about-header">
+        <h1>Our Team</h1>
+        <img src={ourTeamPic} alt="our team" className="ourTeamPic"></img>
+      </div> 
+      <div className="crew-members">
+        {data.map((members) => (
+          <Members  members= {members} key={members.id}/>
+        ))}
+      </div>
+      <div className="our-values">
+        <h1>Our values</h1>
+        <div className="presentation">
+          <div className="text-left">
+            <p>A Handi-Friendly company. Indeed, our Managing Director, Tom himself with a disability. We have arranged the store, with access ramps for wheelchair, very friendly staff listening and sensitized to the cause of disability</p>
+          </div>
+          <div className="pic-container">
+            <img src={handiPic} alt="we are handi-friendly" className="pic"></img>
+          </div>
+        </div>
+        <div className="presentation">
+          <div className="pic-container">
+            <img src={selectionPic} alt="a great selection from the whole world" className="pic"></img>
+          </div>
+          <div className="text-right">
+            <p>A quality selection of vinyls of all kinds from around the world, for your full satisfaction but also to provide you with a product of impeccable quality both musically speaking than the product itself.</p>
+          </div>
+        </div>
+        <div className="presentation">
+          <div className="text-left">
+            <p>Passion, indeed at Vinhills the passion is at the heart of our Steps. And yes at Vinhills, some of us are musicians, DJs, promoters or just in love with music.</p>
+          </div>
+          <div className="pic-container">
+            <img src={expertPic} alt="a passion about sound" className="pic"></img>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
